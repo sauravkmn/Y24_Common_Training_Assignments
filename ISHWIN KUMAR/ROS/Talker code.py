ffb3ad43f07bc4797c16a4e430255d36d19d57c4
Ironwin-15 <wishwinxv@gmail.com>
2025-05-11 18:56:04 +0530

    Add files via upload

---
+++ ISHWIN KUMAR/ROS/Talker code.py
@@ -0,0 +1,26 @@
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class TalkerNode(Node):
+    def __init__(self):
+        super().__init__('talker')
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
+        self.declare_parameter('custom_message', 'Hello World!')
+        timer_period = 1.0  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        custom_message = self.get_parameter('custom_message').get_parameter_value().string_value
+        msg.data = custom_message
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TalkerNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
